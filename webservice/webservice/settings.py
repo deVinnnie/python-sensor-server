98c9a6759da004ab7b8727ce34fc53aa9b04e084
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2016-03-03 16:53:00 +0100

    Make changes to deploy on Apache Webserver

--- webservice/webservice/settings.py
+++ webservice/webservice/settings.py
@@ -93,6 +93,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #'/Home/Documents/Master_thesis/python-sensor-server/webservice/media/'
--- webservice/webservice/wsgi.py
+++ webservice/webservice/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('/var/www/dev/Repos/python-sensor-server/webservice/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webservice.settings")
 
 from django.core.wsgi import get_wsgi_application
