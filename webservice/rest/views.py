7dd87e27e5ccac3b2fc8b1166caafe3230885634
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2016-03-23 12:09:14 +0100

    Fix bug in rendering when adding measurements
    Renderer would crash on wrongly formatted data parameter, causing a HTTP
    500 error.

--- webservice/rest/views.py
+++ webservice/rest/views.py
@@ -328,7 +328,7 @@ class MeasurementViewSet(
         if serializer.is_valid():
             serializer.save()
             headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data,
+            return Response({'measurements': serializer.data},
                             status=status.HTTP_201_CREATED,
                             headers=headers)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
