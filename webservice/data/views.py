496a5a9c77e5c9da80af2c0aacc3f71a869aca18
Matthias <matthias-mombaerts@users.noreply.github.com>
2016-03-25 13:53:52 +0100

    Automatic alerts on refresh

--- webservice/data/views.py
+++ webservice/data/views.py
@@ -46,27 +46,31 @@ def alerts(request):
     """
     Determine Alerts
     """
-    # set = Sensor.objects.all()
-    s = Sensor.objects.get(pk=1)
-    # for s in set:
+    set = Sensor.objects.all()
+    for s in set:
         # dangers = s.measurements.filter(value__gte=10)
         # dangers = dangers.filter(value__lte=10)
-    min = 0
-    max = 23
-    dangers = s.measurements.filter(measurement_type__exact=1)
-    dangers1 = dangers.filter(value__gte=max)
-    dangers2 = dangers.filter(value__lte=min)
-    for m in dangers1:
-        print(m.value)
-        Alert.objects.create(text="Automatic Alert Test", url="", archived=0, company=Company.objects.get(pk=1),
-                             gateway=Gateway.objects.get(pk=1),
-                             sensor=Sensor.objects.get(pk=1))
+        min = 0
+        max = 27.7
+        dangers = s.measurements.filter(measurement_type__exact=1)
+        dangers1 = dangers.filter(value__gte=max)
+        dangers2 = dangers.filter(value__lte=min)
+        for m in dangers1:
+            print(m.value)
+            company = m.sensor_id.gateway.installation.company.company_id
+            gateway = m.sensor_id.gateway.gateway_id
+            sensor = m.sensor_id.sensor_id
+            Alert.objects.create(text="Sensor " + str(sensor) + " has a measurement out of bounds!", url="", archived=0,
+                                 company=Company.objects.get(pk=company),
+                                 gateway=Gateway.objects.get(pk=gateway),
+                                 sensor=Sensor.objects.get(pk=sensor))
 
-    for m in dangers2:
-        print(m.value)
-        Alert.objects.create(text="Automatic Alert Test", url="", archived=0, company=Company.objects.get(pk=1),
-                             gateway=Gateway.objects.get(pk=1),
-                             sensor=Sensor.objects.get(pk=1))
+        for m in dangers2:
+            print(m.value)
+            Alert.objects.create(text="Sensor " + str(sensor) + " has a measurement out of bounds!", url="", archived=0,
+                                 company=Company.objects.get(pk=company),
+                                 gateway=Gateway.objects.get(pk=gateway),
+                                 sensor=Sensor.objects.get(pk=sensor))
 
 
     return HttpResponse("Hello, world. You're at the polls index.")
