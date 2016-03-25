856d3fc5e7074203bdd662d6afe2b9ef02154f4f
Matthias <matthias-mombaerts@users.noreply.github.com>
2016-03-25 15:38:06 +0100

    Update boundaries and use them in creation of alerts
    Also use types in creation of alerts

--- webservice/data/views.py
+++ webservice/data/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
 
-from data.models import Company, Installation, Gateway, Sensor, Alert
+from data.models import Company, Installation, Gateway, Sensor, Alert, MeasurementType
 
 
 class IndexView(generic.ListView):
@@ -46,37 +46,43 @@ def alerts(request):
     """
     Determine Alerts
     """
-    set = Sensor.objects.all()
-    for s in set:
-        # dangers = s.measurements.filter(value__gte=10)
-        # dangers = dangers.filter(value__lte=10)
-        min = 0
-        max = 27.7
-        dangers = s.measurements.filter(measurement_type__exact=1)
-        dangers1 = dangers.filter(value__gte=max)
-        dangers2 = dangers.filter(value__lte=min)
-        for m in dangers1:
-            if m.alert == False:
-                print(m.value)
-                company = m.sensor_id.gateway.installation.company.company_id
-                gateway = m.sensor_id.gateway.gateway_id
-                sensor = m.sensor_id.sensor_id
-                Alert.objects.create(text="Sensor " + str(sensor) + " has a measurement out of bounds!", url="", archived=0,
-                                     company=Company.objects.get(pk=company),
-                                     gateway=Gateway.objects.get(pk=gateway),
-                                     sensor=Sensor.objects.get(pk=sensor))
-                m.alert = True
-                m.save()
 
-        for m in dangers2:
-            if m.alert == False:
-                print(m.value)
-                Alert.objects.create(text="Sensor " + str(sensor) + " has a measurement out of bounds!", url="", archived=0,
-                                     company=Company.objects.get(pk=company),
-                                     gateway=Gateway.objects.get(pk=gateway),
-                                     sensor=Sensor.objects.get(pk=sensor))
-                m.alert = True
-                m.save()
+    measurement_types = MeasurementType.objects.all()
+    for t in measurement_types:
+        min = t.lower_bound
+        max = t.upper_bound
+
+        set = Sensor.objects.all()
+        for s in set:
+            # dangers = s.measurements.filter(value__gte=10)
+            # dangers = dangers.filter(value__lte=10)
+            #min = 0
+            #max = 27.7
+            dangers = s.measurements.filter(measurement_type__exact=t.measurementTypeID)
+            dangers1 = dangers.filter(value__gte=max)
+            dangers2 = dangers.filter(value__lte=min)
+            for m in dangers1:
+                if m.alert == False:
+                    print(m.value)
+                    company = m.sensor_id.gateway.installation.company.company_id
+                    gateway = m.sensor_id.gateway.gateway_id
+                    sensor = m.sensor_id.sensor_id
+                    Alert.objects.create(text="Sensor " + str(sensor) + " has a measurement out of bounds!", url="", archived=0,
+                                         company=Company.objects.get(pk=company),
+                                         gateway=Gateway.objects.get(pk=gateway),
+                                         sensor=Sensor.objects.get(pk=sensor))
+                    m.alert = True
+                    m.save()
+
+            for m in dangers2:
+                if m.alert == False:
+                    print(m.value)
+                    Alert.objects.create(text="Sensor " + str(sensor) + " has a measurement out of bounds!", url="", archived=0,
+                                         company=Company.objects.get(pk=company),
+                                         gateway=Gateway.objects.get(pk=gateway),
+                                         sensor=Sensor.objects.get(pk=sensor))
+                    m.alert = True
+                    m.save()
 
 
     # return HttpResponse("Hello, world. You're at the polls index.")
