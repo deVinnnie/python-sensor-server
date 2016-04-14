b5b0c0642bc17ba826e195ec2d0109b28d070865
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2016-04-14 10:21:20 +0200

    Add Back/Forward button for table

--- webservice/data/static/data/gateway.js
+++ webservice/data/static/data/gateway.js
@@ -1,8 +1,7 @@
 function loadMeasurements(type){
     targetId = "#measurements-type-" + type;
     //$(targetId).empty();
-
-    url = base_url + "?type=" + type + "&start=2016-05-01&end=2016-05-03";
+    url = base_url + "?type=" + type + "&start=" + start.format("YYYY-MM-DD") + "&end=" + end.format("YYYY-MM-DD");
     console.log(url);
 
     $.getJSON(url, function(data) {
@@ -12,48 +11,44 @@ function loadMeasurements(type){
             items.push("<th>Sensor " + value + "</th>" );
         });
 
+        $(targetId + " table thead").empty();
         $(targetId + " table thead").append(items);
 
+        $(targetId + " table tbody").empty();
         $.each(data.measurements.values, function(index, value){
             row = [];
             row.push("<td>" + value.date + "</td>");
 
             $.each(value.data, function(index, value){
-                row.push("<td>" + value + "</td>");
+                if(value != null){
+                    row.push("<td>" + value + "</td>");
+                }
+                else{
+                    row.push("<td></td>");
+                }
             });
 
             $(targetId + " table tbody").append(
-                "<tr>" + row + "</tr>");
-        });
-    });
-
-    /*sensors.forEach(function(s) {
-        url = base_url + s + "/measurements.json?type=" + type + "&start=2016-03-04"
-
-        $.getJSON(url, function(data) {
-            sensor_id = 1;
-            var items = [];
-            $.each( data.measurements, function(index, value) {
-                //console.log(value[1])
-                items.push( "<li>" + value[1] + "</li>" );
-            });
-
-            $(targetId).append(
-                        $("<div/>", { class : "col-md-5"}).append(
-                                                                $("<h3/>", {
-                                                                    text : "Sensor " + s
-                                                                    }))
-                                                          .append(
-                                                                $("<ul/>").append(
-                                                                        items.join("")
-                                                                )
-                                                          )
+                "<tr>" + row + "</tr>"
             );
         });
-    });*/
+    });
 }
 
-$('#measurementDataTabs a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+function loadPrevMeasurements(type){
+    console.log("H");
+    start = start.subtract(20, 'days');
+    end = end.subtract(20, 'days');
+    loadMeasurements(type);
+}
+
+function loadNextMeasurements(type){
+    start = start.add(20, 'days');
+    end = end.add(20, 'days');
+    loadMeasurements(type);
+}
+
+$('#measurementDataTabs a[data-toggle="tab"]').on('shown.bs.tab', function(e){
     console.log(e.target.dataset.measurementType); // newly activated tab
     loadMeasurements(e.target.dataset.measurementType);
 })
