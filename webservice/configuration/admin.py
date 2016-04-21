a921dd1e3314634d99f02112a88fbc114204a5fb
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2016-04-21 21:15:53 +0200

    Remove unused app

--- webservice/configuration/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from configuration.models import (SensorConfiguration, GatewayConfiguration, )
-
-admin.site.register(GatewayConfiguration)
-admin.site.register(SensorConfiguration)
--- webservice/configuration/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import patterns, url
-
-from django.conf import settings
-
-from configuration import views
